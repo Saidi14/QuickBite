e3431d7274b666cee0af8455f3a11689295dc19e
Saidi <sparkinshot@gmail.com>
2026-01-08 19:19:01 +0200

    edit frontend structure

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return(
+        <header className="header">
+            <p>QB</p>
+            <ul className="navlinks">
+                <li>Home</li>
+                <li className = "login">Login</li>
+                <li className = "signup">Sign Up</li>
+            </ul>
+        </header>
+    )
+}
+export default Header
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+
+function Home(){
+    return(
+        <div className="home">
+            <Header/>
+        </div>
+    )
+}
+export default Home
